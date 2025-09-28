4247d0b0b6b420a60ed4272a565ad5f5cfbba1b3
dagsvariedades-hash <dagsvariedades@gmail.com>
2025-09-28 12:57:13 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+videos = {
+    "inicial": [
+        {"titulo": "Bienvenida", "url": "https://www.youtube.com/embed/ID1"},
+        {"titulo": "Primeros Pasos", "url": "https://www.youtube.com/embed/ID2"},
+    ],
+    "servicios": [{"titulo": "Reportes Avanzados", "url": "https://www.youtube.com/embed/ID3"}],
+    "admin":     [{"titulo": "Gestión de Usuarios", "url": "https://www.youtube.com/embed/ID4"}],
+    "usuario":   [{"titulo": "Uso Diario", "url": "https://www.youtube.com/embed/ID5"}],
+    "adminsec":  [{"titulo": "Delegación de Tareas", "url": "https://www.youtube.com/embed/ID6"}],
+}
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/modulo/<name>')
+def modulo(name):
+    if name not in videos:
+        return "Módulo no encontrado", 404
+    return render_template(f'modulo_{name}.html', vids=videos[name], modulo=name.capitalize())
+
+@app.route('/contacto', methods=['GET', 'POST'])
+def contacto():
+    if request.method == 'POST':
+        # Aquí podrías procesar el formulario, enviar correo, etc.
+        return redirect(url_for('home'))
+    return render_template('contacto.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
